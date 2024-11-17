7ac621a80a463b79d76f7ed63aeff94917b50ece
Rajarajan Jayakumar <70993465+Rajarajan3110@users.noreply.github.com>
2024-11-17 23:09:17 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[]{4,5,2,1,6,8,10,3};
+        
+    }
+}
